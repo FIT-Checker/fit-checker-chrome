2f532aab62957a5fc6ff3fd0b272857d15a4d36c
Martin Stekl <martin.stekl@gmail.com>
2013-03-06 15:59:32 +0100

    Cleaned code and used Common API

--- js/main.js
+++ js/main.js
@@ -1,23 +1,30 @@
-// jQuery extension
-$.expr[':'].contentIs = function(el, idx, meta) {
-    return $(el).text() === meta[3];
+var subjects = [];
+var statuses = [];
+var points = [];
+var subjectContents = [];
+
+var application = {
+	inner: {
+		// Browser class instance variable
+		browser: null,
+		username: ''
+	},
+	run: function() {
+		this.inner.browser = browserChrome();
+		this.loadSubjects();
+	},
+	loadSubjects: function() {
+		fitChecker.getSubjectsFromEdux(this.showCoursesMenu, this.showErrorMessage);
+	},
+	showErrorMessage: function(message) {
+		fitChecker.showMessage(message, 'error');
+	},
+	showCoursesMenu: function(courses) {
+		$(courses).each(function (index, course) {
+			console.log(index + ' : ' + course);
+		});
+	}
 };
-
-// Browser class instance variable
-var browser;
-var username;
-
-// Status functions - messages to user
-function hideStatus() {
-    $("div#status").fadeOut();
-}
-function setStatus(text, type) {
-    $(document).scrollTop(0);
-    $("div#status").html(text).addClass(type);
-    if (type == 'ok') {
-        setTimeout(hideStatus, 500);
-    }
-}
 
 // Fill prepared data into the extension DOM
 function fillData(username, subjects, contents, statuses, points) {
@@ -39,11 +46,11 @@ function fillData(username, subjects, contents, statuses, points) {
         }
         htmlList += '<li><a class="tab' + statusClass + '" href="#" id="' +
         subjects[i] + '">' + subjects[i] + '</a></li>';
-        if (contents[i] == null) {
+        if (contents[i] === null) {
             contents[i] = 'Data nejsou k dispozici.';
         }
         pointsContent = '';
-        if (points[i] != null) {
+        if (points[i] !== null) {
             pointsContent = '<div class="points">' + points[i] + '</div>';
         }
         $("div#content").append('<div id="' + subjects[i] +
@@ -54,7 +61,7 @@ function fillData(username, subjects, contents, statuses, points) {
             '</div>');
         $("div#" + subjects[i] + '_content').hide();
     }
-    
+
     browser.getLinksAlive('edux-link');
 
     htmlList += '</ul>';
@@ -63,8 +70,6 @@ function fillData(username, subjects, contents, statuses, points) {
 
     $("a#" + subjects[0]).parent().addClass("current");
     $("div#" + subjects[0] + '_content').show();
-
-    
 
     setStatus('Sta\u017Eeno', 'ok');
 
@@ -84,20 +89,20 @@ function fillData(username, subjects, contents, statuses, points) {
 
 // Parsing logic for each subject
 function parseSubjectData(responseText) {
-    status = null;
+	var status = null;
     sumOfPoints = null;
-    
+
     subjectName = $("a.wikilink2", responseText).attr('href');
     subjectName = subjectName.replace(/\/courses\/([^\/]*)\/.*/, "$1");
     subjCont = $("div.overTable:eq(0)", responseText).html();
-    
-    if (subjCont != null) {
+
+    if (subjCont !== null) {
         // Get rid of unneccessary content
         subjCont = subjCont.replace(/(.*)<thead>.*<\/thead>(.*)/, "$1$2");
         subjCont = subjCont.replace(/(.*)<tr><td>login<\/td>.*<\/tr>(.*)/, "$1$2");
 
         secondTable = $("div.overTable:eq(1)", responseText).html();
-        if (secondTable != null) {
+        if (secondTable !== null) {
             secondTable = secondTable.replace(/(.*)<thead>.*<\/thead>(.*)/, "$1$2");
             subjCont += '<h2><span>Shrnutí</span></h2>' + secondTable;
         }
@@ -110,12 +115,12 @@ function parseSubjectData(responseText) {
             if (status == 'inclusion') {
                 break;
             }
-            
+
             el = $("td:contentIs('" + inclusionStrings[i] + "')", subjCont);
             next = el.next('td');
-            
+
             for (ii = 0; ii < next.length; ii++) {
-                if (next[ii].firstChild != null) {
+                if (next[ii].firstChild !== null) {
                     if ($.inArray(next[ii].firstChild.nodeValue, inclusionValues) != -1) {
                         status = 'inclusion';
                         break;
@@ -125,8 +130,7 @@ function parseSubjectData(responseText) {
         }
 
         // Mark
-        markStrings = ['klasifikovaný zápočet', 'vysledek', 'Známka', 'Znamka' , 
-            'zápočet', 'Zápočet', 'Klasifikace'];
+        var markStrings = ['klasifikovaný zápočet', 'vysledek', 'Známka', 'Znamka' , 'zápočet', 'Zápočet', 'Klasifikace'];
         greenValues = ['A', 'B', 'C', 'D', 'E'];
         for (i = 0; i < markStrings.length; i++) {
             el = $("td:contentIs('" + markStrings[i] + "')", subjCont);
@@ -134,7 +138,7 @@ function parseSubjectData(responseText) {
                 mark = el.next('td');
 
                 for (ii = 0; ii < mark.length; ii++) {
-                    if (mark[ii].firstChild != null) {
+                    if (mark[ii].firstChild !== null) {
                         realMark = mark[ii].firstChild.nodeValue;
                         if ($.inArray(realMark, greenValues) != -1) {
                             status = 'succeed';
@@ -157,7 +161,7 @@ function parseSubjectData(responseText) {
             if (el.length > 0) {
                 next = el.next('td');
                 for (ii = 0; ii < next.length; ii++) {
-                    if (next[ii].firstChild != null) {
+                    if (next[ii].firstChild !== null) {
                         sumOfPoints = next[ii].firstChild.nodeValue;
                     }
                 }
@@ -165,7 +169,7 @@ function parseSubjectData(responseText) {
             }
         }
     }
-    
+
     return {
         'subject': subjectName,
         'status': status,
@@ -173,11 +177,6 @@ function parseSubjectData(responseText) {
         'content': subjCont
     };
 }
-
-var subjects = [];
-var statuses = [];
-var points = [];
-var subjectContents = [];
 
 function downloadByLocalStorage(username, allSubjects, i) {
     xhrIn = new XMLHttpRequest();
@@ -187,8 +186,8 @@ function downloadByLocalStorage(username, allSubjects, i) {
     xhrIn.onreadystatechange = function() {
         if (xhrIn.readyState == 4) {
             result = parseSubjectData(xhrIn.responseText);
-            
-            if (result != null && $.inArray(result.subject, subjects) == -1) {
+
+            if (result !== null && $.inArray(result.subject, subjects) == -1) {
                 subjects.push(result.subject);
                 statuses.push(result.status);
                 points.push(result.points);
@@ -216,13 +215,13 @@ function downloadData(detect) {
     xhr.open("GET", "https://edux.fit.cvut.cz/", true);
     xhr.onreadystatechange = function() {
         if (xhr.readyState == 4) {
-            if (xhr.responseText == '') {
+            if (xhr.responseText === '') {
                 setStatus('Edux je nedostupn\u00FD' +
                     '(<a href="#" onClick="synchronizeData()">' +
                     'zkusit znova</a>)', 'error');
                 return false;
             }
-            
+
             if ($('input[value="Přihlásit se"]', xhr.responseText).length > 0) {
                 setStatus('Nejste p\u0159ihl\u00E1\u0161en/a na <a href="' +
                     'https://edux.fit.cvut.cz/start?do=login" class="link">' +
@@ -230,11 +229,11 @@ function downloadData(detect) {
                 browser.getLinksAlive();
                 return false;
             }
-            
+
             // Get logged user
             username = $("div.user", xhr.responseText).text()
                 .replace(/.*\(([a-z0-9]*)\).*/, "$1");
-            
+
             var xhrM = new XMLHttpRequest();
             xhrM.open("GET","/manifest.json", true);
             xhrM.onreadystatechange = function() {
@@ -244,14 +243,14 @@ function downloadData(detect) {
                 }
             };
             xhrM.send();
-            
+
             $("div#user").hide();
             $("div#user").text(username);
             $("div#user").fadeIn();
 
             setStatus('Stahuji data - 0%', 'loading');
 
-            if (detect == false) {
+            if (detect === false) {
                 allSubjects = JSON.parse(localStorage.getItem('subjects'));
                 downloadByLocalStorage(username, allSubjects, 0);
             } else {
@@ -260,14 +259,14 @@ function downloadData(detect) {
                 allSubjects = $("a[href^=/courses/]", xhr.responseText);
                 allSubjects.each(function(index) {
                     var xhrIn = new XMLHttpRequest();
-                    
+
                     xhrIn.open("GET", "https://edux.fit.cvut.cz" +
                         $(this).attr('href') + "/_export/xhtml/" +
                         "classification/student/" + username + "/start", true);
                     xhrIn.onreadystatechange = function() {
                         if (xhrIn.readyState == 4) {
                             result = parseSubjectData(xhrIn.responseText);
-                            if (result.content != null &&
+                            if (result.content !== null &&
                                     $.inArray(result.subject, subjects) == -1) {
                                 subjects.push(result.subject);
                                 statuses.push(result.status);
@@ -278,7 +277,7 @@ function downloadData(detect) {
 
                             percents = Math.floor(((i/allSubjects.length)*100));
                             setStatus('Stahuji data - ' + percents + '%', 'loading');
-                            
+
                             // Final phase
                             if (i == allSubjects.length) {
                                 fillData(username, subjects, subjectContents,
@@ -292,74 +291,11 @@ function downloadData(detect) {
                 });
             }
         }
-    }
-    xhr.send();
-}
-
-function getSubjectNames () {
-    var savedSubjects = JSON.parse(localStorage.getItem('subjects'));
-    var i = 1;
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://edux.fit.cvut.cz/", true);
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4) {
-            allSubjects = $("a[href^=/courses/]", xhr.responseText);
-            allSubjects.each(function(index) {
-                if ($.inArray($(this).text(), savedSubjects) == -1) {
-                    checked = '';
-                } else {
-                    checked = ' checked="checked"';
-                }
-                $("fieldset").append('<input type="checkbox"' + checked +
-                    ' id="' + $(this).text() + '" /> <label for="' +
-                    $(this).text() + '">' + $(this).text() + '</label><br />');
-                if (index+1 == allSubjects.length) {
-                    $("fieldset").append('<br /><button type="submit" ' +
-                        'onclick="saveForm()">Ulo\u017Eit</button>');
-                    setStatus('Sta\u017Eeno', 'ok');
-                }
-                i++;
-            });
-        }
     };
     xhr.send();
 }
 
-function saveForm() {
-    setStatus('Ukládám...', 'loading');
-    
-    var checkedSubjects = [];
-    $('input[type="checkbox"]:checked').each(function() {
-        checkedSubjects.push($(this).attr("id"));
-    });
-
-    localStorage.setItem('subjects', JSON.stringify(checkedSubjects));
-
-    setStatus('Ulo\u017Eeno', 'ok');
-}
-
-function synchronizeData() {
-    browser = new chromeBrowser;
-    //browser.loginToKos();
-    
-    var savedSubjects = localStorage.getItem('subjects');
-    
-    if (savedSubjects == null) {
-        downloadData(true);
-    } else {
-        downloadData(false);
-    }
-}
-
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-325731-18']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-    var ga = document.createElement('script');
-    ga.type = 'text/javascript';
-    ga.async = true;
-    ga.src = 'https://ssl.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(ga, s);
-})();
+// Run application!
+$(document).ready(function () {
+	application.run();
+});
